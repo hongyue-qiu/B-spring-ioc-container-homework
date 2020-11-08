825d26ec93e053084c048fd90af2fbeb44896478
hongyue-qiu <994059641@qq.com>
2020-11-08 10:48:08 +0800

    homework2 method1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,8 +1,12 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
